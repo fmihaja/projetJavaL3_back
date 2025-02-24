f2153203c21c9d3640901d3858eace5b18e388ef
theLastAirBender <fmihaja041@gmail.com>
2025-02-24 19:53:32 +0100

    Add Order model and establish relationship with Client

--- src/main/java/com/example/myproject/model/Order.java
+++ src/main/java/com/example/myproject/model/Order.java
@@ -1,33 +1,30 @@
 package com.example.myproject.model;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "clients")
+@Table(name = "orders")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Client {
+public class Order {
 
     @Id
     @Column(nullable = false, length = 50)
-    private String email;
+    private String orderId;
 
-    @Column(nullable = false, length = 100)
-    private String name;
+    @Column(nullable = false, columnDefinition = "DATE DEFAULT CURRENT_DATE")
+    private LocalDate orderDate;
 
-    @Column(nullable = true)
-    private String firstName;
-
-    @OneToMany(mappedBy = "client")
-    private List<Order> orders;
+    @ManyToOne
+    private Client client;
 }
