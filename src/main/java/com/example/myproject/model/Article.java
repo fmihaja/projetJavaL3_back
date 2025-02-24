8a196cff60a1edce90886e945b84dbd17726479a
theLastAirBender <fmihaja041@gmail.com>
2025-02-24 20:52:35 +0100

    Refactor Article and Order models to use Long for IDs; add Supplier model with many-to-many relationship to Article

--- src/main/java/com/example/myproject/model/Article.java
+++ src/main/java/com/example/myproject/model/Article.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
@@ -19,15 +21,19 @@ import lombok.NoArgsConstructor;
 public class Article {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, length = 50)
-    private String articleId;
+    private Long articleId;
 
     @Column(nullable = false, length = 100)
     private String name;
 
     @Column(nullable = false)
-    private int prix;
+    private Long prix;
 
     @ManyToMany(mappedBy = "articles")
     private List<Order> orders;
+
+    @ManyToMany(mappedBy = "articles")
+    private List<Supplier> suppliers;
 }
--- src/main/java/com/example/myproject/model/Supplier.java
+++ src/main/java/com/example/myproject/model/Supplier.java
@@ -1,6 +1,5 @@
 package com.example.myproject.model;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import jakarta.persistence.Column;
@@ -9,29 +8,25 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "orders")
+@Table(name = "suppliers")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Order {
+public class Supplier {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
-    private Long orderId;
+    private Long supplierId;
 
-    @Column(nullable = false, columnDefinition = "DATE DEFAULT CURRENT_DATE")
-    private LocalDate orderDate;
-
-    @ManyToOne
-    private Client client;
+    @Column(nullable = false, length = 100)
+    private String name;
 
     @ManyToMany
     private List<Article> articles;
