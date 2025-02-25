0ca5c793492bebea8ebcc2a65ef49c7cf93d9b26
theLastAirBender <fmihaja041@gmail.com>
2025-02-25 12:55:23 +0100

    Remove unused repositories and DTOs for Article, Supplier, Order, and Client

--- src/main/java/com/example/myproject/repository/ArticleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.myproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.myproject.model.Article;
-
-public interface ArticleRepo extends JpaRepository<Article, Long> {
-}
--- src/main/java/com/example/myproject/repository/SupplierRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.myproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.myproject.model.Supplier;
-
-public interface SupplierRepo extends JpaRepository<Supplier, Long> {
-}
